d9982ccc5d053b034f75eaf3496128f03cae11d8
Rob Loach <robloach@gmail.com>
2025-04-26 22:48:09 -0600

    Update

--- Source/index.js
+++ Source/index.js
@@ -6,7 +6,8 @@ const files = [
     'units-civ4.json',
     'units-civ5-vanilla.json',
     'units-civ5-kings.json',
-    'units-rekmod.json'
+    'units-rekmod.json',
+    'units-civ6.json'
 ]
 
 function checkArrayForObjectWithValue(array, property, value) {
@@ -57,11 +58,13 @@ for (let unit of units) {
         colorCount++
     }
 
+    let wiki = `[W](https://en.wikipedia.org/wiki/${unit.name.replaceAll(' ', '%20')})`
+    let civ5custom = `[C](https://civilization-v-customisation.fandom.com/wiki/Special:Search?scope=internal&navigationSearch=true&query=${unit.name.replaceAll(' ', '%20')})`
     
-    output += `| [${unit.name}](https://civilization.fandom.com/wiki/${unit.name.replaceAll(' ', '%20')}) | ${unit.source} | ${image} | ${color1} | ${color2} |\n`
+    output += `| [${unit.name}](https://civilization.fandom.com/wiki/Special:Search?scope=internal&query=${unit.name.replaceAll(' ', '%20')}) ${wiki} ${civ5custom} | ${unit.source} | ${image} | ${color1} | ${color2} |\n`
 }
 
 
-output += `| Total | ${Math.round((imageCount / units.length) * 100)}% | ${Math.round((colorCount / units.length) * 100)}% | ${Math.round((colorCount / units.length) * 100)}% |`
+output += `| **Total** | | ${Math.round((imageCount / units.length) * 100)}% | ${Math.round((colorCount / units.length) * 100)}% | ${Math.round((colorCount / units.length) * 100)}% |`
 
 fs.writeFileSync('../Units.md', output)
